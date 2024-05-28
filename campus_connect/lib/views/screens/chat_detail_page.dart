0f6c516b768421df83f56c3946cfcaee5f7bc8c4
duval-constyl <duvalcon10@gmail.com>
2024-05-28 14:45:26 +0200

    Création des différents fichiers et pages de conception de l'application
    Création des différents fichiers et pages de conception de l'application

---
+++ campus_connect/lib/views/screens/chat_detail_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChatDetailPage extends StatelessWidget {
+  const ChatDetailPage({super.key});
+
+  
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(""),
+      ),
+      body:Container()
+    );
+  }
+}
+ 
---
+++ campus_connect/lib/views/screens/meteo_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MeteoPage extends StatelessWidget {
+  const MeteoPage({super.key});
+
+  
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(""),
+      ),
+      body:Container()
+    );
+  }
+}
+ 
