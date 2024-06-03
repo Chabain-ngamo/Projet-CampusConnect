c8999f3a31be562147700717f364161d02f2eabc
Chabain-ngamo <chabain.ngamo@2025.ucac-icam.com>
2024-06-03 21:26:46 +0100

    connection firebase
    connection firebase

--- campus_connect/lib/main.dart
+++ campus_connect/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:campus_connect/firebase_options.dart';
 import 'package:campus_connect/views/screens/chat_page.dart';
 import 'package:campus_connect/views/screens/notification_page.dart';
 import 'package:campus_connect/views/screens/onboarding_page.dart';
@@ -5,9 +6,13 @@ import 'package:campus_connect/views/screens/profil_page.dart';
 import 'package:campus_connect/views/screens/publication_page.dart';
 import 'package:campus_connect/views/screens/search_page.dart';
 import 'package:campus_connect/views/screens/splash_screen_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
