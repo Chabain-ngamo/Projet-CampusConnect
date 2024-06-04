93d5770cbb049e62ec6919a3eaef0e06686f29bc
Chabain-ngamo <chabain.ngamo@2025.ucac-icam.com>
2024-06-04 04:36:10 +0100

    mise à jour registration / login
    mise à jour registration / login

--- campus_connect/lib/main.dart
+++ campus_connect/lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:campus_connect/firebase_options.dart';
 import 'package:campus_connect/main_navigationbar.dart';
 import 'package:campus_connect/views/screens/onboarding_page.dart';
 import 'package:campus_connect/views/screens/splash_screen_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
